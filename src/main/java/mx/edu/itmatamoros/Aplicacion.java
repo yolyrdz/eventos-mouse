69f791607c651b20d6922d6258ce1fcfdffd27f4
Salvador Rodriguez <sal.rodloy@gmail.com>
2015-10-04 20:50:23 -0600

    primer commit

---
+++ src/main/java/mx/edu/itmatamoros/Aplicacion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.itmatamoros;
+
+/**
+ *
+ * @author salvador
+ */
+public class Aplicacion {
+
+    public static void main(String args[]) {
+        Ventana ventana;
+        ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+}
